82ecf9ee043e22796de15131afb5e39537cc5123
SoloScripter3 <vardhanv6259@gmail.com>
2025-01-13 19:35:25 +0530

    added auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,40 @@
+import * as bcrypt from "bcryptjs";
+import hashPassword from "../utils/hasher.js";
+import User from "../models/users.js";
+import * as jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ username, email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+    const hashedPassword = await hashPassword(password);
+    const newUser = newUser({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    return res.status(500).json({ error: "server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ error: "Invalid credentials" });
+  }
+  const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+    expiresIn: "1h",
+  });
+  res.status(200).json({ token });
+};
